efd4568743969d6136bb040862a6c0b7eab02f40
avijotS <114543519+avijotS@users.noreply.github.com>
2023-08-02 11:34:08 +0530

    binarySearch.c

--- binarySearch.c
+++ binarySearch.c
@@ -1,36 +1,28 @@
-#include<>
-
-
-
-function binarySearch(int arr, int s, int e, int key){
-
-    if(s>=e){
-        return
-    }
-
-    int mid = s+e/2;
-
-    if(key == arr[mid]){
-        return mid;
-    }
-    if(key < arr[mid]){
-        binarySearch(arr, s, mid-1, key)
-    }
-    if(key > arr[mid]){
-        binarySearch(arr, mid+1, e)
-    }
-    return -1
+#include <stdio.h>
+int binarySearch(int arr[], int l, int r, int x)
+{
+	while (l <= r) {
+		int m = l + (r - l) / 2;
+        if (arr[m] == x)
+			return m;
+        if (arr[m] < x)
+			l = m + 1;
+        else
+			r = m - 1;
+	}
+    return -1;
 }
 
 
-int main(){
-
-    int arr = [1,2,3,4,5];
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int key;
-    sort(arr);
-
-    int ans = binarySearch(arr,0,arr.length(), key)
-    fprintf("%d", ans)
-    return 0;
+int main(void){
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int x = 10;
+	int result = binarySearch(arr, 0, n - 1, x);
+	(result == -1) ? printf("Element is not present"
+							" in array")
+				: printf("Element is present at "
+							"index %d",
+							result);
+	return 0;
 }
